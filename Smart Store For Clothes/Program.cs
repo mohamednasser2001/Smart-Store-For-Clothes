6bc1032a0705e01e400669179e6ff10f65dd9c8f
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2026-04-29 15:12:14 +0300

    update

--- Smart Store For Clothes/Program.cs
+++ Smart Store For Clothes/Program.cs
@@ -4,7 +4,6 @@ using DataAccess.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -43,13 +42,10 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+
 using (var scope = app.Services.CreateScope())
 {
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
